65bc3f1dd4f5a84abd8d9fdf6a677d42ee4e0c6d
adityasankar5 <adityasankarsengupta111@gmail.com>
2024-04-09 22:43:03 +0530

    Posts issue

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,7 +1,10 @@
 import { db } from "../connect.js";
 
 export const getPosts = (req, res) => {
-  const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (user.id=p.userId)`;
+  // Use an alias (e.g., 'u') for the users table
+  const q = `SELECT p.*, u.id AS userId, name, profilePic 
+             FROM posts AS p 
+             JOIN users AS u ON (u.id = p.userId)`;
 
   db.query(q, (err, data) => {
     if (err) return res.status(500).json(err);
