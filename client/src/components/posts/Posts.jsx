f7601c62824a9c13ff8b122063102474592af03f
adityasankar5 <adityasankarsengupta111@gmail.com>
2024-04-09 21:45:10 +0530

    Tarnstack v5 adjustments

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,26 +1,30 @@
-
 import Post from "../post/Post";
 import "./posts.scss";
-import {
-  useQuery,
-} from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query';
 import { makeRequest } from "../../axios";
 
 const Posts = () => {
-  
-  const { isLoading, error, data } = useQuery(['posts'], () => 
-  makeRequest.get("/posts").then(res=>{
-    return res.data;
-  })
-);
 
-return (
-  <div className="posts">
-    {data.map((post)=> (
-      <Post post={post} key={post._id} />
-    ))}
+  const options = {
+    queryKey: ['posts'], // Unique identifier for the query
+    queryFn: () => makeRequest.get("/posts").then(res => res.data), // Function to fetch data
+  };
+
+  const { isLoading, error, data } = useQuery(options);
+
+  return (
+    <div className="posts">
+      {isLoading ? (
+        <p>Loading posts...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        data.map((post) => (
+          <Post post={post} key={post._id} />
+        ))
+      )}
     </div>
-);
-    };
+  );
+};
 
 export default Posts;
