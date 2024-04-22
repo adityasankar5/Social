caac3ce79c5ca98c12cd6dbafccdc0ada8308a64
adityasankar5 <adityasankarsengupta111@gmail.com>
2024-04-22 19:05:39 +0530

    api 22/4 b

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUser } from "../controllers/user.js";
+import { getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/find/:userId", getUser);
+router.put("/", updateUser);
 
 router.get("/test", (req, res) => res.send("User route is working"));
 
